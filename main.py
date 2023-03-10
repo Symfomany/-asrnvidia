64bba6e0d1cda66e884d6eb2db95b28cd3d06777
Julien Taiwa <taiwa.julien@gmail.com>
2023-03-10 15:35:21 +0100

    ok

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
-
+from transformers import pipeline
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 app = FastAPI()
+
+pipe_flan = pipeline("text2text-generation", model="google/flan-t5-small")
+
+
+@app.get("/infer_t5")
+def t5(input):
+    output = pipe_flan(input)
+    return {"output": output[0]["generated_text"]}
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+def index() -> FileResponse:
+    return FileResponse(path="/static/index.html", media_type="text/html")
